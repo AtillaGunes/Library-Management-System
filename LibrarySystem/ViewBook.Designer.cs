d5d15da8e80abd307736f1470b40020e810b2d9d
AtillaGunes <81308403+AtillaGunes@users.noreply.github.com>
2022-11-11 06:24:56 +0300

    Book details and management

--- LibrarySystem/ViewBook.Designer.cs
+++ LibrarySystem/ViewBook.Designer.cs
@@ -29,25 +29,25 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtBookName = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtPublication = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtBName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -63,12 +63,13 @@
             this.label1.Text = "BOOK NAME :";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox1
+            // txtBookName
             // 
-            this.textBox1.Location = new System.Drawing.Point(440, 40);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(215, 23);
-            this.textBox1.TabIndex = 1;
+            this.txtBookName.Location = new System.Drawing.Point(440, 40);
+            this.txtBookName.Name = "txtBookName";
+            this.txtBookName.Size = new System.Drawing.Size(215, 23);
+            this.txtBookName.TabIndex = 1;
+            this.txtBookName.TextChanged += new System.EventHandler(this.txtBookName_TextChanged);
             // 
             // dataGridView1
             // 
@@ -79,20 +80,21 @@
             this.dataGridView1.RowTemplate.Height = 25;
             this.dataGridView1.Size = new System.Drawing.Size(810, 319);
             this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.Control;
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnUpdate);
             this.panel1.Controls.Add(this.dateTimePicker1);
-            this.panel1.Controls.Add(this.textBox7);
-            this.panel1.Controls.Add(this.textBox6);
-            this.panel1.Controls.Add(this.textBox4);
-            this.panel1.Controls.Add(this.textBox3);
-            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.txtQuantity);
+            this.panel1.Controls.Add(this.txtPrice);
+            this.panel1.Controls.Add(this.txtPublication);
+            this.panel1.Controls.Add(this.txtAuthor);
+            this.panel1.Controls.Add(this.txtBName);
             this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label5);
@@ -105,55 +107,77 @@
             this.panel1.TabIndex = 3;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // label
+            // btnCancel
             // 
-            this.label.AutoSize = true;
-            this.label.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label.Location = new System.Drawing.Point(49, 41);
-            this.label.Name = "label";
-            this.label.Size = new System.Drawing.Size(91, 21);
-            this.label.TabIndex = 0;
-            this.label.Text = "Book Name";
+            this.btnCancel.Location = new System.Drawing.Point(672, 235);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 15;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
-            // label3
+            // btnDelete
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(49, 113);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 21);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Author";
+            this.btnDelete.Location = new System.Drawing.Point(573, 235);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
-            // label4
+            // btnUpdate
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(49, 168);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(87, 21);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Publication";
+            this.btnUpdate.Location = new System.Drawing.Point(468, 235);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
-            // label5
+            // dateTimePicker1
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(362, 41);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(109, 21);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "Purchase Date";
+            this.dateTimePicker1.Location = new System.Drawing.Point(490, 39);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 12;
             // 
-            // label6
+            // txtQuantity
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(362, 109);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(44, 21);
-            this.label6.TabIndex = 4;
-            this.label6.Text = "Price";
+            this.txtQuantity.Location = new System.Drawing.Point(490, 166);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(162, 23);
+            this.txtQuantity.TabIndex = 11;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(490, 109);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(162, 23);
+            this.txtPrice.TabIndex = 10;
+            // 
+            // txtPublication
+            // 
+            this.txtPublication.Location = new System.Drawing.Point(158, 170);
+            this.txtPublication.Name = "txtPublication";
+            this.txtPublication.Size = new System.Drawing.Size(162, 23);
+            this.txtPublication.TabIndex = 8;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(158, 111);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(162, 23);
+            this.txtAuthor.TabIndex = 7;
+            // 
+            // txtBName
+            // 
+            this.txtBName.Location = new System.Drawing.Point(158, 43);
+            this.txtBName.Name = "txtBName";
+            this.txtBName.Size = new System.Drawing.Size(162, 23);
+            this.txtBName.TabIndex = 6;
             // 
             // label7
             // 
@@ -166,83 +190,65 @@
             this.label7.Text = "Quantity";
             this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
-            // button1
+            // label6
             // 
-            this.button1.Location = new System.Drawing.Point(699, 38);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "SEARCH";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(362, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 21);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Price";
             // 
-            // textBox2
+            // label5
             // 
-            this.textBox2.Location = new System.Drawing.Point(158, 43);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(162, 23);
-            this.textBox2.TabIndex = 6;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(362, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 21);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Purchase Date";
             // 
-            // textBox3
+            // label4
             // 
-            this.textBox3.Location = new System.Drawing.Point(129, 111);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(162, 23);
-            this.textBox3.TabIndex = 7;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(49, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 21);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Publication";
             // 
-            // textBox4
+            // label3
             // 
-            this.textBox4.Location = new System.Drawing.Point(129, 166);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(162, 23);
-            this.textBox4.TabIndex = 8;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(49, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 21);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Author";
             // 
-            // textBox6
+            // label
             // 
-            this.textBox6.Location = new System.Drawing.Point(490, 109);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(162, 23);
-            this.textBox6.TabIndex = 10;
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label.Location = new System.Drawing.Point(49, 41);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(91, 21);
+            this.label.TabIndex = 0;
+            this.label.Text = "Book Name";
             // 
-            // textBox7
+            // btnClear
             // 
-            this.textBox7.Location = new System.Drawing.Point(490, 166);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(162, 23);
-            this.textBox7.TabIndex = 11;
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(490, 39);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
-            this.dateTimePicker1.TabIndex = 12;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(468, 235);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "Update";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(573, 235);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 14;
-            this.button3.Text = "Delete";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(672, 235);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 15;
-            this.button4.Text = "Cancel";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnClear.Location = new System.Drawing.Point(699, 38);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // ViewBook
             // 
@@ -251,10 +257,10 @@
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.AntiqueWhite;
             this.ClientSize = new System.Drawing.Size(960, 732);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnClear);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtBookName);
             this.Controls.Add(this.label1);
             this.Name = "ViewBook";
             this.Text = "ViewBook";
@@ -270,24 +276,24 @@
         #endregion
 
         private Label label1;
-        private TextBox textBox1;
+        private TextBox txtBookName;
         private DataGridView dataGridView1;
         private Panel panel1;
-        private Button button4;
-        private Button button3;
-        private Button button2;
+        private Button btnCancel;
+        private Button btnDelete;
+        private Button btnUpdate;
         private DateTimePicker dateTimePicker1;
-        private TextBox textBox7;
-        private TextBox textBox6;
-        private TextBox textBox4;
-        private TextBox textBox3;
-        private TextBox textBox2;
+        private TextBox txtQuantity;
+        private TextBox txtPrice;
+        private TextBox txtPublication;
+        private TextBox txtAuthor;
+        private TextBox txtBName;
         private Label label7;
         private Label label6;
         private Label label5;
         private Label label4;
         private Label label3;
         private Label label;
-        private Button button1;
+        private Button btnClear;
     }
 }
--- LibrarySystem/ViewBook.cs
+++ LibrarySystem/ViewBook.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace LibrarySystem
 {
@@ -39,7 +42,124 @@ namespace LibrarySystem
 
         private void ViewBook_Load(object sender, EventArgs e)
         {
+            panel1.Visible = false;
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "data source = DESKTOP-5666BR8\\SQLEXPRESS; databese = Library; integrated security = True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from NewBook";
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
 
+        }
+
+
+        int bid;
+        Int64 rowid;
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null) ;
+            {
+                bid = int.Parse(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            }
+            panel1.Visible = true;
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "data source = DESKTOP-5666BR8\\SQLEXPRESS; databese = Library; integrated security = True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+
+            cmd.CommandText = "select * from NewBook where bid";
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            rowid = Int64.Parse(ds.Tables[0].Rows[0][0].ToString());
+
+            txtBName.Text = ds.Tables[0].Rows[0][1].ToString();
+            txtAuthor.Text = ds.Tables[0].Rows[0][2].ToString();
+            txtPublication.Text = ds.Tables[0].Rows[0][3].ToString();
+            dateTimePicker1.Text = ds.Tables[0].Rows[0][4].ToString();
+            txtPrice.Text = ds.Tables[0].Rows[0][5].ToString();
+            txtQuantity.Text = ds.Tables[0].Rows[0][6].ToString();
+
+
+
+
+
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+
+        }
+
+        private void txtBookName_TextChanged(object sender, EventArgs e)
+        {
+            if(txtBookName.Text != "")
+            {
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "data source = DESKTOP-5666BR8\\SQLEXPRESS; databese = Library; integrated security = True";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "select * from NewBook where bName LIKE '"+txtBookName+"%'";
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "data source = DESKTOP-5666BR8\\SQLEXPRESS; databese = Library; integrated security = True";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "select * from NewBook";
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtBookName.Clear();
+            panel1.Visible = false;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            String bName = txtBName.Text;
+            String bauthor = txtAuthor.Text;
+            String publication = txtPublication.Text;
+            String pdate = dateTimePicker1.Text;
+            Int64 price = Int64.Parse(txtPrice.Text);
+            Int64 quan = Int64.Parse(txtQuantity.Text);
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "data source = DESKTOP-5666BR8\\SQLEXPRESS; databese = Library; integrated security = True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "update NewBook set bName = '" + bName + "',bAuthor= '" + bauthor + "',bPubl= '" + pdate + "',bPrice= " + price + ",bQuan= " + quan +" where bid = " +rowid+ "";
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "data source = DESKTOP-5666BR8\\SQLEXPRESS; databese = Library; integrated security = True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "delete from NewBook where bid = " + rowid + "";
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            da.Fill(ds);
         }
     }
 }
